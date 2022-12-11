d7afd308346c47c0a637771a5a29881752b75936
Vincent Schneider <vincent.schneider@sudile.com>
2022-12-11 13:31:44 -0800

    optimizing

--- day-11/day-11.v
+++ day-11/day-11.v
@@ -12,6 +12,33 @@ mut:
 	op         rune
 }
 
+fn (mut o Operation) init(input string) {
+	for i in input {
+		match i {
+			`+`, `-`, `*` {
+				o.op = i
+				break
+			}
+			else {
+				continue
+			}
+		}
+	}
+	parts := input.split(o.op.str())
+	if parts[0] == 'old' {
+		o.left = 0
+		o.isleftold = true
+	} else {
+		o.left = parts[0].i64()
+	}
+	if parts[1] == 'old' {
+		o.right = 0
+		o.isrightold = true
+	} else {
+		o.right = parts[1].i64()
+	}
+}
+
 fn (o Operation) calc(worry_level i64) i64 {
 	match o.op {
 		`+` {
@@ -42,36 +69,9 @@ fn (o Operation) calc(worry_level i64) i64 {
 	}
 }
 
-fn (mut o Operation) init(input string) {
-	for i in input {
-		match i {
-			`+`, `-`, `*` {
-				o.op = i
-				break
-			}
-			else {
-				continue
-			}
-		}
-	}
-	parts := input.split(o.op.str())
-	if parts[0] == 'old' {
-		o.left = 0
-		o.isleftold = true
-	} else {
-		o.left = parts[0].i64()
-	}
-	if parts[1] == 'old' {
-		o.right = 0
-		o.isrightold = true
-	} else {
-		o.right = parts[1].i64()
-	}
-}
-
 struct Monkey {
+	id int
 mut:
-	id            int
 	items         []i64
 	operation     Operation
 	divisible     i64
@@ -80,9 +80,18 @@ mut:
 	inspect_count i64
 }
 
+fn (mut m Monkey) init(input string) {
+	lines := input.split('\n')
+	m.items = lines[1].replace(' ', '').split(':')[1].split(',').map(it.i64())
+	m.operation.init(lines[2].replace(' ', '').split('=')[1])
+	m.divisible = lines[3].split(' ').last().i64()
+	m.pass_true = lines[4].split(' ').last().int()
+	m.pass_false = lines[5].split(' ').last().int()
+}
+
 fn (mut m Monkey) round(mut monkeys []Monkey, lcm i64, impl bool) {
+	m.inspect_count += m.items.len
 	for mut item in m.items {
-		m.inspect_count++
 		if impl {
 			item = m.operation.calc(item) / 3
 		} else {
@@ -97,20 +106,10 @@ fn (m Monkey) pass_to_monkey(item i64) int {
 	return if item % m.divisible == 0 { m.pass_true } else { m.pass_false }
 }
 
-fn (mut m Monkey) init(id int, input string) {
-	m.id = id
-	lines := input.split('\n')
-	m.items = lines[1].replace(' ', '').split(':')[1].split(',').map(it.i64())
-	m.operation.init(lines[2].replace(' ', '').split(':')[1].split('=')[1])
-	m.divisible = lines[3].split(' ').last().i64()
-	m.pass_true = lines[4].split(' ').last().int()
-	m.pass_false = lines[5].split(' ').last().int()
-}
-
 fn main() {
 	if os.args.len != 2 {
-		println('usage: ./day-09 part-1')
-		println('usage: ./day-09 part-2')
+		println('usage: ./day-11 part-1')
+		println('usage: ./day-11 part-2')
 	} else {
 		solution(os.args[1] == 'part-1')
 	}
@@ -121,8 +120,10 @@ fn solution(solution_one bool) {
 	mut monkeys := []Monkey{}
 	mut gcd_l := []i64{}
 	for i, monkey_raw in monkeys_raw {
-		mut m := Monkey{}
-		m.init(i, monkey_raw)
+		mut m := Monkey{
+			id: i
+		}
+		m.init(monkey_raw)
 		monkeys << m
 		gcd_l << m.divisible
 	}
